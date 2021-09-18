f8e9c0413915a2a74882eaf9286dfcdfe3253ce6
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2021-09-18 10:04:33 -0700

    Add files via upload

---
+++ VideoRecorder.py
@@ -0,0 +1,24 @@
+#**** ---- Note --- ****#
+# Video Recorder - Python
+# By Faizanscommunit
+# MIT Licensed
+
+#**** --- Social Links --- ****#
+#  Github: https://github.com/faizanscommunit
+#  Fiverr: https://fvrr.co/3iZIX0L
+#  Website: https://faizanscommunit.pythonanywhere.com/
+#  Instagram: https://www.instagram.com/faizanscommunit/
+
+#**** --- Source Code --- ****#
+import cv2
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+writer = cv2.VideoWriter(
+    'recorded_video.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+while True:
+    _, frame = cap.read()
+    writer.write(frame)
+    cv2.imshow('Video Recorder', frame)
+    if cv2.waitKey(10) == ord('q'):
+        break
